720f2dc4844df3e4a8b7dc108a46bae8d26f847e
rudra <rudra.gujarathi123@gmail.com>
2020-04-17 13:37:37 +0530

    repository created

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+s=socket.socket()
+n=input("Enter the code given from server machine : ")
+host=n
+port=9999
+s.connect((host,port))
+while True:
+    x=input("Client : ")
+    if x =="quit" or not x:
+        print("Quitting")
+        s.close()
+        break
+    s.sendall(x.encode())
+    data=s.recv(1024).decode()
+    if data =="quit" or not data:
+        print("Quitting")
+        s.close()
+        break
+    print("Server : ",data)
+
---
+++ server.py
@@ -0,0 +1,24 @@
+import socket
+s=socket.socket()
+host=socket.gethostname()
+port=9999
+s.bind((host,port))
+s.listen()
+print("Listening...")
+print("Enter this code in client machine :",host)
+conn,addr=s.accept()
+while True:
+    data=conn.recv(1024).decode()
+    if data == "quit" or not data:
+        print("Quitting")
+        conn.close()
+        s.close()
+        break
+    print("Client : ",data)
+    x=input("Server : ")
+    if x =="quit" or not x:
+        print("Quitting")
+        conn.close()
+        s.close()
+        break
+    conn.sendall(x.encode())
